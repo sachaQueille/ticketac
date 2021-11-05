30f410732dca7cccad5358729735b7db69515f23
Annaëlle OVIDE <ovianna@hotmail.fr>
2021-11-05 10:40:50 +0100

    pop-up ok

--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,10 @@ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+document.getElementById('popUp').addEventListener("click",
+   function(){
+     console.log("click détecté !");
+    window.setTimeout(function () {
+      location.href = "../home";
+  }, 5000);
+   })
