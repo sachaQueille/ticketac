0d36b9cce313e79bda49e0dd710af01306a158af
Sacha <s.hauberdon@gmail.com>
2021-11-05 09:03:15 +0100

    bdd ok, orders ok, lastrtips ok

--- models/users.js
+++ models/users.js
@@ -1,12 +1,21 @@
 let mongoose = require('mongoose');
 
+let tripsSchema = mongoose.Schema({
+    departure: String,
+    arrival: String,
+    date: Date,
+    departureTime: String,
+    price: Number,
+})
+
 let userSchema = mongoose.Schema({
     firstName: String,
     lastName: String,
     email: String,
     pwd: String,
+    trips: [tripsSchema],
 })
 
-let UserModel = mongoose.model('users', userSchema)
+let UserModel = mongoose.model('users', userSchema);
 
 module.exports = {UserModel}
--- routes/index.js
+++ routes/index.js
@@ -16,8 +16,8 @@ router.get('/home', function(req, res, next) {
   res.render('home', { title: 'Express' });
 });
 
-router.get('/results', function(req, res, next) {
-  res.render('results', { title: 'Express' });
+router.get('/results', async (req, res, next) => {
+  res.render('results', { title: 'Express', resultSearch: req.session.searchResult });
 });
 
 router.get('/noresults', function(req, res, next) {
@@ -25,7 +25,17 @@ router.get('/noresults', function(req, res, next) {
 });
 
 router.get('/order', function(req, res, next) {
-  res.render('order', { title: 'Express' });
+	if (typeof(req.session.shop) == "undefined")  {
+		req.session.shop = [];
+	}
+  let i = req.query.position;
+
+  req.session.shop.push(
+		req.session.searchResult[i]
+	);
+
+
+  res.render('order', {result: req.session.shop , username: req.session.firstname });
 });
 
 router.get('/trips', function(req, res, next) {
@@ -39,18 +49,20 @@ router.post('/sign-up', async(req, res, next) => {
     lastName: req.body.lastName,
     email: req.body.mail,
     pwd: req.body.pwd,
+    tripsUser: [],
   })
   
   let checkMail = await UserModel.findOne({email: req.body.mail});
   
-  console.log(checkMail);
-  //console.log(userSaved)
+  
   if(checkMail == null) {
-    let userSaved = await newUser.save();
-    req.session.userName = userSaved.firstName;
-    //console.log(req.session.name);
-    req.session.userId = userSaved._id;
-    //console.log(req.session)
+    var userSaved = await newUser.save();
+    req.session.userid = userSaved._id;
+    req.session.firstname = userSaved.firstname;
+    req.session.lastname = userSaved.lastname;
+    req.session.email = userSaved.email;
+    req.session.tripsUser = userSaved.tripsUser;
+    
     res.redirect('/home');
   } else {
     res.redirect('/');
@@ -63,8 +75,11 @@ router.post('/sign-in', async(req, res, next) => {
   let userExist = await UserModel.findOne({mail: req.body.emailUser, pwd: req.body.pwdUser});
 
   if(userExist) {
-    req.session.userName = userExist.firstName;
-    req.session.userID = userExist._id;
+    req.session.userid = userExist._id;
+    req.session.firstname = userExist.firstname;
+    req.session.lastname = userExist.lastname;
+    req.session.email = userExist.email;
+    req.session.tripsUser = userExist.tripsUser;
     res.redirect('/home');
   } else {
     res.redirect('/');
@@ -73,14 +88,67 @@ router.post('/sign-in', async(req, res, next) => {
 });
 
 router.post('/search', async function(req, res, next) {
-  let trajet = await journeyModel.find({departure: req.body.cityfrom, arrival: req.body.cityto, date:req.body.date})
-  console.log(trajet)
-  if(trajet.length > 0){
+  let resultSearch = await journeyModel.find({departure: req.body.cityfrom, arrival: req.body.cityto, date:req.body.date})
+  
+  let date = req.body.date; 
+
+  if(resultSearch.length > 0){
+
+    let results = [];
+
+    for(let i = 0; i < resultSearch.length; i++) {
+      results.push({
+        cityfrom: req.body.cityfrom,
+				cityto: req.body.cityto,
+				date: req.body.date,
+				hour: resultSearch[i].departureTime,
+				price: Number( resultSearch[i].price ),
+      });
+    }
+    req.session.searchResult = results;
+   
     res.redirect("/results")
   } else {
-    res.redirect("/noresults",)
+    res.redirect("/noresults")
   }
 }); 
+
+router.get('/updatebdd', async (req, res, next) => {
+
+	let user = await UserModel.findOne( {_id : req.session.userid});
+
+	
+	for (let i=0; i<req.session.shop.length; i++) {
+		user.trips.push({
+			departure: req.session.shop[i].cityfrom,
+		  	arrival: req.session.shop[i].cityto,
+		  	date: req.session.shop[i].date,
+		  	departureTime: req.session.shop[i].hour,
+		  	price: req.session.shop[i].price,
+		});
+	}
+	
+	await user.save(function (err) {
+  		if (err) return handleError(err);
+  		console.log('Success!');
+	});
+	
+	res.render('home', { username: req.session.firstname });
+});
+
+router.get('/lasttrips', async (req, res, next) => {
+
+	let user = await UserModel.findOne( {_id : req.session.userid} );
+
+	let lastTrips = user.trips;
+
+	res.render('lasttrips', { lastTrips,  username: req.session.firstname });
+});
+
+router.get('/logout', function (req, res, next) {
+  req.session.destroy();
+   res.redirect('/');
+ });
 
 // Remplissage de la base de donnée, une fois suffit
 router.get('/save', async function(req, res, next) {
