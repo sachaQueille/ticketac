886cacd17275537ce8cc980f786e4f4c91b59bc3
Sacha <s.hauberdon@gmail.com>
2021-11-05 14:43:18 +0100

    redirect home ok

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,12 @@ var date = ["2018-11-20","2018-11-21","2018-11-22","2018-11-23","2018-11-24"];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login');
+  if(req.session.userid) {
+    res.render('home')
+  } else {
+    res.render('login')
+  }
+
 });
 
 router.get('/home', function(req, res, next) {
