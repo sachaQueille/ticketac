b0a15f6cac861e2db7d5a6f279553c7d071d8e53
Sacha <s.hauberdon@gmail.com>
2021-11-05 11:30:48 +0100

    clear github

---
+++ public/javascripts/scripts.js
@@ -0,0 +1,12 @@
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+
+})
+document.getElementById('popUp').addEventListener("click",
+   function(){
+     console.log("click détecté !");
+    window.setTimeout(function () {
+      location.href = "/updatebdd";
+  }, 5000);
+   }) 
